0b33335e94e86750bc63cbeeaa94b3ae65ed1b35
Taeer Bar-Yam <taeer@bar-yam.me>
2022-03-16 13:21:55 -0400

    switch to uberjar

--- src/build.clj
+++ src/build.clj
@@ -1,22 +1,21 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'my/lib1)
-(def version (format "1.2.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def uber-file (format "target/uber.jar"))
 
 (defn clean [_]
   (b/delete {:path "target"}))
 
-(defn jar [_]
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis basis
-                :src-dirs ["src"]})
+(defn uber [_]
+  (clean nil)
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file jar-file}))
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'main}))
---
+++ src/main.clj
@@ -0,0 +1,5 @@
+(ns main
+    (:gen-class))
+
+(defn -main [& args]
+  (println "Hello, world!"))
