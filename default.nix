738f26f8e921c17641e59e90c1bf9bf7ad64c262
Taeer Bar-Yam <taeer@bar-yam.me>
2022-03-17 12:19:27 -0400

    add readme

--- default.nix
+++ default.nix
@@ -6,16 +6,16 @@ let
     #   sha256 = "0g0jn8cp1f3zgs7xk2xb2vwa44gb98qlp7k0dvigs0zh163c2kim";
     # };
     # this commit works
-    builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz";
-      sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-    };
+    # builtins.fetchTarball {
+    #   url = "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz";
+    #   sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+    # };
     # first bad commit
     # clojure: 1.10.3.855 -> 1.10.3.933
-    # builtins.fetchTarball {
-    #   url = "https://github.com/NixOS/nixpkgs/archive/fea6b0e497fbda44ad1afd66286dde8e2151f0d7.tar.gz";
-    #   sha256 = "1973f3f4hv1rz6jvq51f4kqh64in3nwn7im3bwwh5p0imaf5kahs";
-    # };
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/fea6b0e497fbda44ad1afd66286dde8e2151f0d7.tar.gz";
+      sha256 = "1973f3f4hv1rz6jvq51f4kqh64in3nwn7im3bwwh5p0imaf5kahs";
+    };
 in
 with import nixpkgs {};
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+commands to reproduce:
+
+```
+nix-shell
+clojure -X:build uber
+nix-build
+```
+
