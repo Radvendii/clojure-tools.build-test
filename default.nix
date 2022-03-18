ae205cb1da4f182a356c5bcd9d0ec9f7829f501b
Taeer Bar-Yam <taeer@bar-yam.me>
2022-03-18 09:22:11 -0400

    .m2 generation

--- default.nix
+++ default.nix
@@ -6,19 +6,72 @@ let
     #   sha256 = "0g0jn8cp1f3zgs7xk2xb2vwa44gb98qlp7k0dvigs0zh163c2kim";
     # };
     # this commit works
-    # builtins.fetchTarball {
-    #   url = "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz";
-    #   sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-    # };
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/7e9b0dff974c89e070da1ad85713ff3c20b0ca97.tar.gz";
+      sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+    };
     # first bad commit
     # clojure: 1.10.3.855 -> 1.10.3.933
-    builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/fea6b0e497fbda44ad1afd66286dde8e2151f0d7.tar.gz";
-      sha256 = "1973f3f4hv1rz6jvq51f4kqh64in3nwn7im3bwwh5p0imaf5kahs";
-    };
+    # builtins.fetchTarball {
+    #   url = "https://github.com/NixOS/nixpkgs/archive/fea6b0e497fbda44ad1afd66286dde8e2151f0d7.tar.gz";
+    #   sha256 = "1973f3f4hv1rz6jvq51f4kqh64in3nwn7im3bwwh5p0imaf5kahs";
+    # };
 in
 with import nixpkgs {};
+let
+  packages = (pkgs.callPackage (import ./deps.nix) { }).packages;
+  pom = { packages, name }:
+    let deps = lib.filterAttrs (n: v: builtins.elem name v.dependents) packages; in
+      pkgs.writeText "pom.xml" ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+      <modelVersion>4.0.0</modelVersion>
+      <groupId>${packages.${name}.groupId}</groupId>
+      <artifactId>${packages.${name}.artifactId}</artifactId>
+      <packaging>jar</packaging>
+      <version>${packages.${name}.version}</version>
+      ${lib.optionalString (deps != {}) "<dependencies>"}
+      ${lib.concatStringsSep "\n  "
+        (lib.mapAttrsToList (n: {dependents, artifactId, groupId, version, ...}: ''
+          <dependency>
+            <groupId> ${groupId} </groupId>
+            <artifactId> ${artifactId} </artifactId>
+            <version> ${version} </version>
+          </dependency>
+        '') deps)
+       }
+      ${lib.optionalString (deps != {}) "</dependencies>"}
+    </project>
+  '';
+  mvnRepo = linkFarm "mvn-repo" (lib.flatten (lib.mapAttrsToList (name: {src, artifactId, version, groupId, ...}: [
+    {
+      name = "${builtins.replaceStrings ["."] ["/"] groupId}/${artifactId}/${version}/${artifactId}-${version}.jar";
+      path = src.jar;
+    }
+    {
+      name = "${builtins.replaceStrings ["."] ["/"] groupId}/${artifactId}/${version}/${artifactId}-${version}.pom";
+      path = pom { inherit packages name; };
+    }
+  ]) (lib.filterAttrs (n: v: v.src ? jar) packages)));
 
+  # localRoot = name:
+  #   let inherit (packages.${name}) src artifactId version groupId; in
+  #   linkFarm "mvn-folder-${name}" [
+  #     {
+  #       name = "${artifactId}-${version}.jar";
+  #       path = src.jar;
+  #     }
+  #     {
+  #       name = "pom.xml";
+  #       path = pom { inherit packages name; };
+  #     }
+  #   ];
+
+  # cljJank =
+  #   ''{:aliases {:jank {:extra-deps {${lib.concatStringsSep " " (lib.mapAttrsToList (name: _: ''${name} {:local/root "${localRoot name}" :deps/manifest :pom}'') (lib.filterAttrs (_: v: v.src ? jar) packages))}}}}}} '';
+in
+
+# mvnRepo
 stdenv.mkDerivation rec {
 
   name = "tools.build-test";
@@ -31,12 +84,15 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     export HOME=/build
-    cp -r ${~/.m2} $HOME/.m2
-    chmod -R +w $HOME/.m2
+    mkdir -p $HOME/.m2
+    ln -s ${mvnRepo} $HOME/.m2/repository
     cp -r ${~/.gitlibs} $HOME/.gitlibs
     chmod -R +w $HOME/.gitlibs
 
-    # strace -e 'trace=%file' -y clojure -X:build uber
+    # strace -f -e 'trace=%file' -y clojure -X:build uber
     clojure -X:build uber
+    # clojure -Sdeps '${"" /* cljJank */}' -X:build:jank uber
+    mkdir -p $out/bin
+    mv target/uber.jar $out/bin/${name}.jar
   '';
 }
